556c8929f9ec09c982c7d4138675d97dbbacbdef
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2016-05-26 09:51:24 -0500

    Mejorando la calidad de la cámara

--- js/index.js
+++ js/index.js
@@ -76,7 +76,7 @@ $(document).ready(function(){
 					        	$("#mensajes").fadeOut(1500).removeClass("alert-danger");
 					        }, 5000);
 						}, { 
-							quality: 20,
+							quality: 50,
 							destinationType: Camera.DestinationType.FILE_URI,
 							targetWidth: 250,
 							targetHeight: 250,
